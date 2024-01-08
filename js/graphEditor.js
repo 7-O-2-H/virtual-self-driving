3ecc6fb7ddff18f01b57080c94d1049d136d011b
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-08 14:57:54 -0500

    highlight selected points

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -7,16 +7,21 @@ class GraphEditor {
 
     this.#addEventListeners();
 
+    this.selected = null;
   }
-  
+
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (e) => {
       const mouse = new Point(e.offsetX, e.offsetY);
       this.graph.addPoint(mouse);
+      this.selected = mouse;
     });
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true });
+    }
   }
 }
