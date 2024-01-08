c6f54279b4aa33cbf41103a0c408ec2b9fefdbd7
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-08 14:51:19 -0500

    add clickable option to add points

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,6 +4,16 @@ class GraphEditor {
     this.graph = graph;
 
     this.ctx = this.canvas.getContext("2d");
+
+    this.#addEventListeners();
+
+  }
+  
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", (e) => {
+      const mouse = new Point(e.offsetX, e.offsetY);
+      this.graph.addPoint(mouse);
+    });
   }
 
   display() {
