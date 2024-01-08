be79f56a20ba02250d2f36ab3a30e18f320696ae
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-08 17:08:14 -0500

    remove point with rigth click

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -14,20 +14,27 @@ class GraphEditor {
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (e) => {
       const mouse = new Point(e.offsetX, e.offsetY);
-      this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-      if (this.hovered) {
-        this.selected = this.hovered;
-        return;
+      if(e.button === 2) { // rigth click
+        if (this.hovered) {
+          this.graph.removePoint(this.hovered);
+        }
       }
-      this.graph.addPoint(mouse);
-      this.selected = mouse;
+      if (e.button === 0) {
+        if (this.hovered) {
+          this.selected = this.hovered;
+          return;
+        }
+        this.graph.addPoint(mouse);
+        this.selected = mouse;
+      }
     });
     this.canvas.addEventListener("mousemove", (e) => {
       const mouse = new Point(e.offsetX, e.offsetY);
       this.hovered = getNearestPoint(mouse, this.graph.points, 10);
     });
+    this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
   }
-
+  
   display() {
     this.graph.draw(this.ctx);
     if (this.hovered) {
