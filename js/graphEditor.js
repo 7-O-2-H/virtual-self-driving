8296d40b20053cb5cd2dbbd87bb6dcc80545f5b7
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-08 17:21:22 -0500

    add dragging of points

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -9,6 +9,7 @@ class GraphEditor {
 
     this.selected = null;
     this.hovered = null;
+    this.dragging = false;
   }
 
   #addEventListeners() {
@@ -22,23 +23,33 @@ class GraphEditor {
       if (e.button === 0) {
         if (this.hovered) {
           this.selected = this.hovered;
+          this.dragging = true;
           return;
         }
         this.graph.addPoint(mouse);
         this.selected = mouse;
+        this.hovered = mouse;
       }
     });
     this.canvas.addEventListener("mousemove", (e) => {
       const mouse = new Point(e.offsetX, e.offsetY);
       this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+      if (this.dragging === true) {
+        this.selected.x = mouse.x;
+        this.selected.y = mouse.y;
+      }
     });
     this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.canvas.addEventListener("mouseup", () => this.dragging = false);
+
   }
 
   #removePoint(point) {
     this.graph.removePoint(point);
     this.hovered = null;
-    this.selected = null;
+    if (this.selected === point) {
+      this.selected = null;
+    }
   }
 
   display() {
