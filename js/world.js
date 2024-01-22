0d735722a1784c869302d6e0c57d1cda477d93c8
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-22 14:06:41 -0500

    add road lines

--- js/world.js
+++ js/world.js
@@ -23,11 +23,11 @@ class World {
 
   draw(ctx) {
     for (const env of this.envelopes) {
-      env.draw(ctx, { fill: "#BBB", stroke: "#BBB"});
+      env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
     }
     for (const seg of this.graph.segments) {
-      seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
-   }
+      seg.draw(ctx, { color: "white", width: 4, dash: [10,10] })
+    }
     for (const seg of this.roadBorders) {
       seg.draw(ctx, { color: "white", width: 4 });
     }
