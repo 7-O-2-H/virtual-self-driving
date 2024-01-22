fad31a9e0e91ab9eb809e59d8652b595939e8109
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-22 13:48:34 -0500

    remove highlighted, overlapping segments

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -12,7 +12,7 @@ class Segment {
     return this.p1.equals(point) || this.p2.equals(point);
   }
 
-  draw(ctx, { width = 2, color = "black", dash = [] } = {}) {
+  draw(ctx, { width = 3, color = "black", dash = [] } = {}) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.strokeStyle = color;
