d1f26360f04865987b6f42d89bbb8e862caad991
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-23 18:00:14 -0500

    update to tree class, make trees green'

---
+++ js/items/tree.js
@@ -0,0 +1,15 @@
+class Tree {
+  constructor(center, size) {
+    this.center = center;
+    this.size = size; //base of tree
+  }
+
+  draw(ctx) {
+    c//onst diff = subtract(this.center, viewPoint);
+
+    this.center.draw(ctx, { size: this.size, color: "green" })
+
+    // const top = add(this.center, diff);
+    // new Segment(this.center, top).draw(ctx);
+  }
+}
