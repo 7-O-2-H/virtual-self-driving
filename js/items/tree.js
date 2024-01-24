188222157cc7d05e814937860bc1c752722903db
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-24 18:29:48 -0500

    pseudo 3D tree shell

--- js/items/tree.js
+++ js/items/tree.js
@@ -8,10 +8,15 @@ class Tree {
   draw(ctx, viewPoint) {
 
     const diff = subtract(this.center, viewPoint);
+    const top = add(this.center, scale(diff, this.heigthCoef));
 
-    this.center.draw(ctx, { size: this.size, color: "green" })
-
-    const top = add(this.center, diff)
-    new Segment(this.center, top).draw(ctx);
+    const levelCount = 7;
+    for (let level = 0; level < levelCount; level++) {
+      const t = level / (levelCount - 1);
+      const point = lerp2D(this.center, top, t);
+      const color = "rgb(30," + lerp(50, 200, t) + ",70)";
+      const size = lerp(this.size, 40, t);
+      point.draw(ctx, { size, color });
+    }
   }
 }
