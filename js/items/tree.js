14ce1811deea370f0934fd2a1f18314c161424c0
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-24 18:11:08 -0500

    add viewpoint dependency for trees

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,14 +1,17 @@
 class Tree {
-  constructor(center, size) {
+  constructor(center, size, heigthCoef = 0.3) {
     this.center = center;
     this.size = size; //base of tree
+    this.heigthCoef = heigthCoef;
   }
 
-  draw(ctx) {
+  draw(ctx, viewPoint) {
+
+    const diff = subtract(this.center, viewPoint);
 
     this.center.draw(ctx, { size: this.size, color: "green" })
 
-    const top = add(this.center, { x: - 40, y: -40 })
+    const top = add(this.center, diff)
     new Segment(this.center, top).draw(ctx);
   }
 }
