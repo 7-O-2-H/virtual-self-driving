39ee7809c99c6bdd3663c9b59d7114ed8d12af10
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-24 18:40:32 -0500

    implemented 3d trees with layers of polygons

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,8 +1,20 @@
 class Tree {
-  constructor(center, size, heigthCoef = 0.3) {
+  constructor(center, size, heigthCoef = 0.25) {
     this.center = center;
     this.size = size; //base of tree
     this.heigthCoef = heigthCoef;
+    this.base = this.#generateLevel(center, size);
+  }
+
+  #generateLevel(point, size) {
+    const points = [];
+    const rad = size / 2;
+    for (let a = 0; a < Math.PI * 2; a += Math.PI / 16) {
+      const kindOfRandom = Math.cos(((a + this.center.x) * size) % 17) ** 2;
+      const noisyRadius = rad * lerp(0.5, 1, kindOfRandom);
+      points.push(translate(point, a, noisyRadius));
+    }
+    return new Polygon(points);
   }
 
   draw(ctx, viewPoint) {
@@ -10,13 +22,15 @@ class Tree {
     const diff = subtract(this.center, viewPoint);
     const top = add(this.center, scale(diff, this.heigthCoef));
 
-    const levelCount = 7;
+    const levelCount = 8;
     for (let level = 0; level < levelCount; level++) {
       const t = level / (levelCount - 1);
       const point = lerp2D(this.center, top, t);
-      const color = "rgb(30," + lerp(50, 200, t) + ",70)";
+      const color = "rgb(30," + lerp(50, 180, t) + ",70)";
       const size = lerp(this.size, 40, t);
-      point.draw(ctx, { size, color });
+      const poly = this.#generateLevel(point, size);
+      poly.draw(ctx, { fill: color, stroke: "rgba(0,0,0,0)" });
     }
+    //this.base.draw(ctx);
   }
-}
+};
